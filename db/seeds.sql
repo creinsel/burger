d54d86e80ae50d91c741be3a02c629e452e2e515
Cassondra Reinsel <cassiereinsel@gmail.com>
2020-02-11 11:13:17 -0800

    schema and seeds

---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('Fatty Patty', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Slap Yo Mama', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Cassie So Sassy', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Mc Double', true);
