76a815253fa0d8765d6cb920fc634e636aab8733
Cassondra Reinsel <cassiereinsel@gmail.com>
2020-02-12 08:47:39 -0800

    made orm file

---
+++ config/orm.js
@@ -0,0 +1,15 @@
+var connection = require("../config/connection.js");
+
+var orm = {
+    selectAll: function(whatToSelect, ){
+        var queryStr = "SELECT ?? FROM ??"
+    },
+
+    insertOne: function(){
+        //more stuff here
+    },
+
+    updateOne: function(){
+        //put things here
+    }
+}
