ccc53cc3ad536604087a34877600922050c382ee
Cassondra Reinsel <cassiereinsel@gmail.com>
2020-03-08 14:39:08 -0700

    changed schema

--- db/schema.sql
+++ db/schema.sql
@@ -4,8 +4,7 @@ USE burgers_db;
 
 CREATE TABLE burgers
 (
-	id int NOT NULL,
+	id  SERIAL PRIMARY KEY,
 	burger_name varchar(255) NOT NULL,
-	devoured BOOLEAN DEFAULT false,
-	PRIMARY KEY (id)
+	devoured BOOLEAN DEFAULT false
 );
